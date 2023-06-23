bbf439de0918ad317eae906f5bfde42b5d0b73ad
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-06-23 13:45:52 -0300

    intial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openModalLogin() {
+    document.getElementById("login").style.display = "block";
+}
+function openModalRegister() {
+    document.getElementById("register").style.display = "block";
+}
+function closeModalLogin() {
+    document.getElementById("login").style.display = "none";
+}
+function closeModalRegister() {
+    document.getElementById("register").style.display = "none";
+}
+function redirigir(pagina) {
+    window.location.href = pagina;
+}
